c76eb3cb1a0b48b3fb55218422fdd4f0e0212f48
Benjamin Cruttenden <cruttenden@cs.ucsb.edu>
2023-05-10 14:48:30 -0700

    bc - phone table ready

--- src/test/java/edu/ucsb/cs156/example/controllers/SchoolsControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/SchoolsControllerTests.java
@@ -3,8 +3,8 @@ package edu.ucsb.cs156.example.controllers;
 import edu.ucsb.cs156.example.repositories.UserRepository;
 import edu.ucsb.cs156.example.testconfig.TestConfig;
 import edu.ucsb.cs156.example.ControllerTestCase;
-import edu.ucsb.cs156.example.entities.Phone;
-import edu.ucsb.cs156.example.repositories.PhoneRepository;
+import edu.ucsb.cs156.example.entities.Schools;
+import edu.ucsb.cs156.example.repositories.SchoolsRepository;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,50 +30,50 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@WebMvcTest(controllers = PhonesController.class)
+@WebMvcTest(controllers = SchoolsController.class)
 @Import(TestConfig.class)
-public class PhonesControllerTests extends ControllerTestCase {
+public class SchoolsControllerTests extends ControllerTestCase {
 
         @MockBean
-        PhoneRepository phoneRepository;
+        SchoolsRepository schoolsRepository;
 
         @MockBean
         UserRepository userRepository;
 
-        // Authorization tests for /api/phones/admin/all
+        // Authorization tests for /api/schools/admin/all
 
         @Test
         public void logged_out_users_cannot_get_all() throws Exception {
-                mockMvc.perform(get("/api/phones/all"))
+                mockMvc.perform(get("/api/schools/all"))
                                 .andExpect(status().is(403)); // logged out users can't get all
         }
 
         @WithMockUser(roles = { "USER" })
         @Test
         public void logged_in_users_can_get_all() throws Exception {
-                mockMvc.perform(get("/api/phones/all"))
+                mockMvc.perform(get("/api/schools/all"))
                                 .andExpect(status().is(200)); // logged
         }
 
         @Test
         public void logged_out_users_cannot_get_by_id() throws Exception {
-                mockMvc.perform(get("/api/phones?id=7"))
+                mockMvc.perform(get("/api/schools?id=7"))
                                 .andExpect(status().is(403)); // logged out users can't get by id
         }
 
-        // Authorization tests for /api/phones/post
+        // Authorization tests for /api/schools/post
         // (Perhaps should also have these for put and delete)
 
         @Test
         public void logged_out_users_cannot_post() throws Exception {
-                mockMvc.perform(post("/api/phones/post"))
+                mockMvc.perform(post("/api/schools/post"))
                                 .andExpect(status().is(403));
         }
 
         @WithMockUser(roles = { "USER" })
         @Test
         public void logged_in_regular_users_cannot_post() throws Exception {
-                mockMvc.perform(post("/api/phones/post"))
+                mockMvc.perform(post("/api/schools/post"))
                                 .andExpect(status().is(403)); // only admins can post
         }
 
@@ -85,22 +85,22 @@ public class PhonesControllerTests extends ControllerTestCase {
 
                 // arrange
 
-                Phone phone = Phone.builder()
-                                .brand("Apple")
-                                .model("iPhone 14")
-                                .price(799)
+                Schools school = Schools.builder()
+                                .name("Isla Vista Elementary School")
+                                .district("Goleta Union School District")
+                                .gradeRange("K-6")
                                 .build();
 
-                when(phoneRepository.findById(eq(7L))).thenReturn(Optional.of(phone));  // Check not sure why id is 7
+                when(schoolsRepository.findById(eq(7L))).thenReturn(Optional.of(school));
 
                 // act
-                MvcResult response = mockMvc.perform(get("/api/phones?id=7"))
+                MvcResult response = mockMvc.perform(get("/api/schools?id=7"))
                                 .andExpect(status().isOk()).andReturn();
 
                 // assert
 
-                verify(phoneRepository, times(1)).findById(eq(7L));
-                String expectedJson = mapper.writeValueAsString(phone);
+                verify(schoolsRepository, times(1)).findById(eq(7L));
+                String expectedJson = mapper.writeValueAsString(school);
                 String responseString = response.getResponse().getContentAsString();
                 assertEquals(expectedJson, responseString);
         }
@@ -111,152 +111,152 @@ public class PhonesControllerTests extends ControllerTestCase {
 
                 // arrange
 
-                when(phoneRepository.findById(eq(7L))).thenReturn(Optional.empty());
+                when(schoolsRepository.findById(eq(7L))).thenReturn(Optional.empty());
 
                 // act
-                MvcResult response = mockMvc.perform(get("/api/phones?id=7"))
+                MvcResult response = mockMvc.perform(get("/api/schools?id=7"))
                                 .andExpect(status().isNotFound()).andReturn();
 
                 // assert
 
-                verify(phoneRepository, times(1)).findById(eq(7L));
+                verify(schoolsRepository, times(1)).findById(eq(7L));
                 Map<String, Object> json = responseToJson(response);
                 assertEquals("EntityNotFoundException", json.get("type"));
-                assertEquals("Phone with id 7 not found", json.get("message"));
+                assertEquals("Schools with id 7 not found", json.get("message"));
         }
 
         @WithMockUser(roles = { "USER" })
         @Test
-        public void logged_in_user_can_get_all_phones() throws Exception {
+        public void logged_in_user_can_get_all_schools() throws Exception {
 
                 // arrange
 
-                Phone phone1 = Phone.builder()
-                                .brand("Apple")
-                                .model("iPhone 14")
-                                .price(799)
+                Schools school1 = Schools.builder()
+                                .name("Isla Vista Elementary School")
+                                .district("Goleta Union School District")
+                                .gradeRange("K-6")      
                                 .build();
 
-                Phone phone2 = Phone.builder()
-                                .brand("Samsung")
-                                .model("Galaxy S22")
-                                .price(699)
+                Schools school2 = Schools.builder()
+                                .name("Carpinteria High School")
+                                .district("Carpinteria Unified School District")
+                                .gradeRange("9-12") 
                                 .build();
 
-                ArrayList<Phone> expectedPhones = new ArrayList<>();
-                expectedPhones.addAll(Arrays.asList(phone1, phone2));
+                ArrayList<Schools> expectedSchools = new ArrayList<>();
+                expectedSchools.addAll(Arrays.asList(school1, school2));
 
-                when(phoneRepository.findAll()).thenReturn(expectedPhones);
+                when(schoolsRepository.findAll()).thenReturn(expectedSchools);
 
                 // act
-                MvcResult response = mockMvc.perform(get("/api/phones/all"))
+                MvcResult response = mockMvc.perform(get("/api/schools/all"))
                                 .andExpect(status().isOk()).andReturn();
 
                 // assert
 
-                verify(phoneRepository, times(1)).findAll();
-                String expectedJson = mapper.writeValueAsString(expectedPhones);
+                verify(schoolsRepository, times(1)).findAll();
+                String expectedJson = mapper.writeValueAsString(expectedSchools);
                 String responseString = response.getResponse().getContentAsString();
                 assertEquals(expectedJson, responseString);
         }
 
         @WithMockUser(roles = { "ADMIN", "USER" })
         @Test
-        public void an_admin_user_can_post_a_new_phone() throws Exception {
+        public void an_admin_user_can_post_a_new_school() throws Exception {
                 // arrange
 
-                Phone phone1 = Phone.builder()
-                                .brand("Apple")
-                                .model("iPhoneX")
-                                .price(999)
+                Schools school1 = Schools.builder()
+                                .name("Isla Vista Elementary School")
+                                .district("Goleta Union School District")
+                                .gradeRange("K-6")  
                                 .build();
 
-                when(phoneRepository.save(eq(phone1))).thenReturn(phone1);
+                when(schoolsRepository.save(eq(school1))).thenReturn(school1);
 
                 // act
                 MvcResult response = mockMvc.perform(
-                                post("/api/phones/post?brand=Apple&qmodel=iPhoneX&price=999")
+                                post("/api/schools/post?name=Isla Vista Elementary School&district=Goleta Union School District&gradeRange=K-6")
                                                 .with(csrf()))
                                 .andExpect(status().isOk()).andReturn();
 
                 // assert
-                verify(phoneRepository, times(1)).save(phone1);
-                String expectedJson = mapper.writeValueAsString(phone1);
+                verify(schoolsRepository, times(1)).save(school1);
+                String expectedJson = mapper.writeValueAsString(school1);
                 String responseString = response.getResponse().getContentAsString();
                 assertEquals(expectedJson, responseString);
         }
 
         @WithMockUser(roles = { "ADMIN", "USER" })
         @Test
-        public void admin_can_delete_a_date() throws Exception {
+        public void admin_can_delete_a_school() throws Exception {
                 // arrange
 
-                Phone phone1 = Phone.builder()
-                                .brand("Google")
-                                .model("Pixel 6")
-                                .price(599)
+                Schools school1 = Schools.builder()
+                                .name("Isla Vista Elementary School")
+                                .district("Goleta Union School District")
+                                .gradeRange("K-6")  
                                 .build();
 
-                when(phoneRepository.findById(eq(15L))).thenReturn(Optional.of(phone1));
+                when(schoolsRepository.findById(eq(15L))).thenReturn(Optional.of(school1));
 
                 // act
                 MvcResult response = mockMvc.perform(
-                                delete("/api/phones?id=15")
+                                delete("/api/schools?id=15")
                                                 .with(csrf()))
                                 .andExpect(status().isOk()).andReturn();
 
                 // assert
-                verify(phoneRepository, times(1)).findById(15L);
-                verify(phoneRepository, times(1)).delete(any());
+                verify(schoolsRepository, times(1)).findById(15L);
+                verify(schoolsRepository, times(1)).delete(any());
 
                 Map<String, Object> json = responseToJson(response);
-                assertEquals("Phone with id 15 deleted", json.get("message"));
+                assertEquals("School with id 15 deleted", json.get("message"));
         }
 
         @WithMockUser(roles = { "ADMIN", "USER" })
         @Test
-        public void admin_tries_to_delete_non_existant_phone_and_gets_right_error_message()
+        public void admin_tries_to_delete_non_existant_school_and_gets_right_error_message()
                         throws Exception {
                 // arrange
 
-                when(phoneRepository.findById(eq(15L))).thenReturn(Optional.empty());
+                when(schoolsRepository.findById(eq(15L))).thenReturn(Optional.empty());
 
                 // act
                 MvcResult response = mockMvc.perform(
-                                delete("/api/phones?id=15")
+                                delete("/api/schools?id=15")
                                                 .with(csrf()))
                                 .andExpect(status().isNotFound()).andReturn();
 
                 // assert
-                verify(phoneRepository, times(1)).findById(15L);
+                verify(schoolsRepository, times(1)).findById(15L);
                 Map<String, Object> json = responseToJson(response);
-                assertEquals("Phone with id 15 not found", json.get("message"));
+                assertEquals("Schools with id 15 not found", json.get("message"));
         }
 
         @WithMockUser(roles = { "ADMIN", "USER" })
         @Test
-        public void admin_can_edit_an_existing_phone() throws Exception {
+        public void admin_can_edit_an_existing_school() throws Exception {
                 // arrange
 
-                Phone phoneOrig = Phone.builder()
-                                .brand("LG")
-                                .model("Q8")
-                                .price(699)
+                Schools schoolOrig = Schools.builder()
+                                .name("Isla Vista Elementary School")
+                                .district("Goleta Union School District")
+                                .gradeRange("K-6") 
                                 .build();
 
-                Phone phoneEdited = Phone.builder()
-                                .brand("Motorola")
-                                .model("G5S")
-                                .price(599)
+                Schools schoolEdited = Schools.builder()
+                                .name("Goleta Valley Junior High School")
+                                .district("Santa Barbara Unified School District")
+                                .gradeRange("7-8") 
                                 .build();
 
-                String requestBody = mapper.writeValueAsString(phoneEdited);
+                String requestBody = mapper.writeValueAsString(schoolEdited);
 
-                when(phoneRepository.findById(eq(67L))).thenReturn(Optional.of(phoneOrig));
+                when(schoolsRepository.findById(eq(67L))).thenReturn(Optional.of(schoolOrig));
 
                 // act
                 MvcResult response = mockMvc.perform(
-                                put("/api/phones?id=67")
+                                put("/api/schools?id=67")
                                                 .contentType(MediaType.APPLICATION_JSON)
                                                 .characterEncoding("utf-8")
                                                 .content(requestBody)
@@ -264,31 +264,30 @@ public class PhonesControllerTests extends ControllerTestCase {
                                 .andExpect(status().isOk()).andReturn();
 
                 // assert
-                verify(phoneRepository, times(1)).findById(67L);
-                verify(phoneRepository, times(1)).save(phoneEdited); // should be saved with correct user
+                verify(schoolsRepository, times(1)).findById(67L);
+                verify(schoolsRepository, times(1)).save(schoolEdited); // should be saved with correct user
                 String responseString = response.getResponse().getContentAsString();
                 assertEquals(requestBody, responseString);
         }
 
         @WithMockUser(roles = { "ADMIN", "USER" })
         @Test
-        public void admin_cannot_edit_phone_that_does_not_exist() throws Exception {
+        public void admin_cannot_edit_school_that_does_not_exist() throws Exception {
                 // arrange
 
-                Phone editedPhone = Phone.builder()
-                                .brand("Samsung")
-                                .model("Galaxy Note 10")
-                                .price(1099)
+                Schools schoolEdited = Schools.builder()
+                                .name("Isla Vista Elementary School")
+                                .district("Goleta Union School District")
+                                .gradeRange("K-6") 
                                 .build();
 
+                String requestBody = mapper.writeValueAsString(schoolEdited);
 
-                String requestBody = mapper.writeValueAsString(editedPhone);
-
-                when(phoneRepository.findById(eq(67L))).thenReturn(Optional.empty());
+                when(schoolsRepository.findById(eq(67L))).thenReturn(Optional.empty());
 
                 // act
                 MvcResult response = mockMvc.perform(
-                                put("/api/phones?id=67")
+                                put("/api/schools?id=67")
                                                 .contentType(MediaType.APPLICATION_JSON)
                                                 .characterEncoding("utf-8")
                                                 .content(requestBody)
@@ -296,9 +295,9 @@ public class PhonesControllerTests extends ControllerTestCase {
                                 .andExpect(status().isNotFound()).andReturn();
 
                 // assert
-                verify(phoneRepository, times(1)).findById(67L);
+                verify(schoolsRepository, times(1)).findById(67L);
                 Map<String, Object> json = responseToJson(response);
-                assertEquals("Phone with id 67 not found", json.get("message"));
+                assertEquals("Schools with id 67 not found", json.get("message"));
 
         }
 }

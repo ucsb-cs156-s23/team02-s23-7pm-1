acd47ac369233d37f2259f37524aebedaa644e6a
Cameron Barrett <cameronbarrett@ucsb.edu>
2023-05-09 22:24:13 -0700

    whoops forget these files in last commit

--- src/main/java/edu/ucsb/cs156/example/repositories/MajorRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/MajorRepository.java
@@ -1,12 +1,12 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.Majors;
+import edu.ucsb.cs156.example.entities.Major;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface MajorRepository extends CrudRepository<Majors, Long> {
-  Iterable<Majors> findAllByDepartment(String department);
+public interface MajorRepository extends CrudRepository<Major, Long> {
+  Iterable<Major> findAllByDepartment(String department);
 }
